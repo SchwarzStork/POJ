14e88c08047c1633548c00fb16d9458adbe47318
Aleksander Mojzych <32591117+SchwarzStork@users.noreply.github.com>
2019-05-10 22:21:03 +0200

    Add files via upload

---
+++ POJ-Zjazd5/src/EXERCISES04_01.java
@@ -0,0 +1,108 @@
+/*
+Cw 4
+Zad 1
+http://szuflandia.pjwstk.edu.pl/~pczapiewski/POJ_2018-2019/POJ_cwiczenia_04_2019.04.27.pdf
+*/
+
+class Person {
+
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String toString(){
+        return "Person[name = " + name + ", address =" + address + "]";
+    }
+}
+
+
+class Student extends Person{
+
+    private String program;
+    private int yeaar;
+    private double fee;
+
+    public Student(String name, String address, String program, int year, double fee){
+        super(name, address);
+        this.program = program;
+        this.yeaar = year;
+        this.fee = fee;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public int getYeaar() {
+        return yeaar;
+    }
+
+    public void setYeaar(int yeaar) {
+        this.yeaar = yeaar;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    public String toString(){
+        return "Student[Person[name = " + super.getName() + ", address =" + super.getAddress() + "],program = " + program +", year = " + yeaar + "]";
+    }
+}
+
+
+class Staff extends Person{
+
+    private String school;
+    private double pay;
+
+    public Staff(String name, String address, String school, double pay) {
+        super(name, address);
+        this.school = school;
+        this.pay = pay;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    public String toString(){
+        return "Staff[Person[name = " + super.getName() + ", address =" + super.getAddress() + "],school = " + school +", pay = " + pay + "]";
+    }
+}
---
+++ POJ-Zjazd5/src/EXERCISES04_02.java
@@ -0,0 +1,95 @@
+/*
+Cw 4
+Zad 2
+http://szuflandia.pjwstk.edu.pl/~pczapiewski/POJ_2018-2019/POJ_cwiczenia_04_2019.04.27.pdf
+*/
+
+class Point2D {
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    public Point2D() {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        float coords[] = new float[2];
+        coords[0] = x;
+        coords[1] = y;
+        return coords;
+    }
+
+    public String toString(){
+        return "(" + x + "," + y + ")";
+    }
+}
+
+class Point3D extends Point2D{
+
+    private float z = 0.0f;
+
+    public Point3D() {
+        super();
+        this.z = z;
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        super.setX(x);
+        super.setY(y);
+        this.z = z;
+    }
+
+    public float[] getXYZ(){
+        float coords[] = new float[3];
+        coords[0] = super.getX();
+        coords[1] = super.getY();
+        coords[2] = z;
+        return coords;
+    }
+
+    public String toString(){
+        return "(" + super.getX() + "," + super.getY() + "," + z +")";
+    }
+
+}
