2067d01cf87266e34fbe6bf2e2cbcf05f3a986e2
Aleksander Mojzych <32591117+SchwarzStork@users.noreply.github.com>
2019-05-25 15:22:44 +0200

    first upload

---
+++ src/Runner/DemoRunners.java
@@ -0,0 +1,12 @@
+/*
+Autorzy: Aleksander Mojzych, Artur Dembicki  25.05.2019
+ */
+
+package Runner;
+
+public class DemoRunners {
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/Runner/Runner.java
@@ -0,0 +1,10 @@
+/*
+Autorzy: Aleksander Mojzych, Artur Dembicki  25.05.2019
+ */
+
+package Runner;
+
+public interface Runner {
+
+     void run();
+}
